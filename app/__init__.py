217ffe2ad993425a99392fa416999ec34e39007f
shelkanife <shelkanife884@gmai.com>
2022-02-08 21:22:13 -0600

    m method completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from app.mmethod import start
+import json
 
 app=Flask(__name__)
 
@@ -8,4 +10,8 @@ def index():
 
 @app.route('/m-method',methods=['POST'])
 def m_method():
-    ...
+    data_json=request.get_json()
+    problem=json.loads(data_json['toSendProblem'])
+    dataProblem=json.loads(data_json['dataProblem'])
+    response=start(problem,dataProblem)
+    return json.dumps(response)
--- app/static/js/problem.js
+++ app/static/js/problem.js
@@ -1,3 +1,5 @@
+const TABLES=document.getElementById('tables')
+
 function generateSelectMinMax(){
     let select=document.createElement('select')
     select.id='minMax'
@@ -14,7 +16,7 @@ function generateSelectMinMax(){
 
 function generateSelectOperators(){
     let select=document.createElement('select')
-    select.id='lge'
+    select.className='lge'
     let optionLe=document.createElement('option')
     optionLe.value='le'
     optionLe.textContent='<='
@@ -44,6 +46,53 @@ function generateObjectiveFunction(variables){
     }
     return p
 }
+
+function generteTable(problem,heading,nconstrains,varHolgura){
+    // console.log('generateTable ', varHolgura)
+    let p=document.createElement('p')
+    p.innerHTML=`${varHolgura[0]}`
+    let table = document.createElement('table')
+    let trHeading = document.createElement('tr')
+    table.appendChild(trHeading)
+    let thVariablesTemplate ='<th></th>'
+    for(let i of heading[0]){
+        thVariablesTemplate += `<th>${i}</th>`
+    }
+
+    thVariablesTemplate += '<th>Z</th>'
+    trHeading.innerHTML = thVariablesTemplate
+    let trObjective = document.createElement('tr')
+    let trObjectiveTemplate = '<td></td>'
+    for(let i of problem[0])
+        trObjectiveTemplate += `<td>${i}</td>`
+    trObjective.innerHTML = trObjectiveTemplate
+    table.appendChild(trObjective)
+    let templateConstrains=''
+    for(i=0;i<nconstrains;i++){
+        let trConstrains = document.createElement('tr')
+        table.appendChild(trConstrains)
+        templateConstrains = `<td>${varHolgura[1][i]}</td>`
+        for(let item of problem[1][i])
+            templateConstrains += `<td>${item}</td>`
+        trConstrains.innerHTML = templateConstrains
+    }
+    p.append(table)
+    return p
+}
+
+function makeBaseMatriz(variables,constrains){
+    let aVariables = [],aConstrains = []
+    aVariables.push(Array.from(variables).map(variable => parseInt(variable.value)))
+    let aAux=Array.from(constrains).map(constrain => parseInt(constrain.value))
+    
+    const SLICE = variables.length+1 
+    for (let i = 0; i < aAux.length; i += SLICE) 
+        aConstrains.push(aAux.slice(i, i + SLICE))
+
+    return aVariables.concat(aConstrains)
+}
+
+const getOperators= (collection) => Array.from(collection).map(element => element.value)
 
 function createProblem(layout,variables,constrains){
     layout.appendChild(generateObjectiveFunction(variables))
@@ -76,5 +125,46 @@ function createProblem(layout,variables,constrains){
 }
 
 function send(e){
+    const VARIABLE = document.getElementsByClassName('variable')
+    const CONSTRAIN = document.getElementsByClassName('constrain')
+    let problemType=document.querySelector('#minMax').value
+    let variables=parseInt(document.querySelector('#variables').value)
+    let constrains=parseInt(document.querySelector('#constrains').value)
+    let problem=makeBaseMatriz(VARIABLE,CONSTRAIN)
     
+    let data=[]
+    data.push(problemType)
+    data.push(variables)
+    data.push(constrains)
+    data.push(getOperators(document.getElementsByClassName('lge')))
+    let body=JSON.stringify({
+        "dataProblem":JSON.stringify(data),
+        "toSendProblem":JSON.stringify(problem)
+    })
+    fetch('https://mmethodflask.herokuapp.com/m-method',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body
+    })
+    .then(async res =>{
+        let response=await res.json()
+
+        TABLES.innerHTML=''
+        let tb=document.createElement('h1')
+        tb.textContent='Tablón Base'
+        TABLES.appendChild(tb)
+        TABLES.appendChild(generteTable(response[0][0],response[2],constrains,response[3][0]))
+        for(let i=1;i<response[0].length;i++){
+            let tbi=document.createElement('h1')
+            tbi.textContent=`Tablón ${i}`
+            TABLES.appendChild(tbi)
+            TABLES.appendChild(generteTable(response[0][i],response[2],constrains,response[3][i]))
+        }
+        alert(`Z= ${response[1]}`)
+    })
+
+
+
+
+
 }
